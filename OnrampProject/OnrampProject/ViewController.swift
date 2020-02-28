e97b39291a1990bee803b4eb982ca37d8ad61554
bart <bartngonzalez@gmail.com>
2020-02-28 15:28:08 -0800

    added OnrampProject starting files (template).

---
+++ OnrampProject/OnrampProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnrampProject
+//
+//  Created by Giovanni Noa on 2/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
