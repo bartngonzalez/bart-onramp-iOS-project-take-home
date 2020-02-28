5256ac2919b80ead2fd1f0a5b905d24a436e990d
bart <bartngonzalez@gmail.com>
2020-02-28 15:57:06 -0800

    @available(iOS 13.0, *) for AppDelegate and SceneDelegate.

--- OnrampProject/OnrampProject/AppDelegate.swift
+++ OnrampProject/OnrampProject/AppDelegate.swift
@@ -7,11 +7,10 @@
 
 import UIKit
 
+@available(iOS 13.0, *)
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
