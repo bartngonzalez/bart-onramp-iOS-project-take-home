586a2c66fe8cafc4510b385ccae8d071e68b7e0b
bart <bartngonzalez@gmail.com>
2020-03-04 02:16:49 -0800

    NyNews request user location. Will use lattitude and longitude to show news articles near the users current location.

--- OnrampProject/OnrampProject/ViewControllers/MyNewsVC.swift
+++ OnrampProject/OnrampProject/ViewControllers/MyNewsVC.swift
@@ -6,12 +6,70 @@
 //
 
 import UIKit
+import CoreLocation
 
-class MyNewsVC: UITableViewController {
+class MyNewsVC: UITableViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    var latitude: String = "0.000000"
+    var longitude: String = "0.000000"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("MyNewsVC: viewDidLoad()")
+        
+        locationManager.delegate = self
+        checkLocationAuthorization()
+    }
+    
+    func checkLocationAuthorization() {
+        
+        print("checkLocationAuthorization()")
+        
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            print(".authorizedWhenInUse")
+            
+            if latitude == "0.000000" || longitude == "0.000000" {
+                print("startUpdatingLocation()")
+                locationManager.startUpdatingLocation()
+            } else {
+                print("stopUpdatingLocation")
+                locationManager.stopUpdatingLocation()
+            }
+        case .denied:
+            print(".denied")
+            locationManager.requestWhenInUseAuthorization()
+        case .notDetermined:
+            print(".notDetermined")
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            print("restricted")
+        default:
+            print("default")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        print("locationManager(): didUpdateLocations")
+        
+        locationManager.stopUpdatingLocation()
+        
+        latitude = String(format: "%f", locationManager.location?.coordinate.latitude ?? "0.000000")
+        longitude = String(format: "%f", locationManager.location?.coordinate.longitude ?? "0.000000")
+        
+        print("lat: \(latitude)")
+        print("lon: \(longitude)")
+        
+        checkLocationAuthorization()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        print("locationManager(): didChangeAuthorization")
+        
+        checkLocationAuthorization()
     }
 }
