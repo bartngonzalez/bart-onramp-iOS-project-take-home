3dde1b3c163edee786e72262819811782ec51bb1
bart <bartngonzalez@gmail.com>
2020-03-04 00:10:03 -0800

    Changed storyboard Personal to MyNews

---
+++ OnrampProject/OnrampProject/ViewControllers/MyNewsVC.swift
@@ -0,0 +1,17 @@
+//
+//  MyNewsVC.swift
+//  OnrampProject
+//
+//  Created by Bart on 3/3/20.
+//
+
+import UIKit
+
+class MyNewsVC: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MyNewsVC: viewDidLoad()")
+    }
+}
